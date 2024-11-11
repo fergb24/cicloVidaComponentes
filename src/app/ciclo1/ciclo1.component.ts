40ad8f22327e17b0dab27045b47a54b63f558c64
Fernando García <fernandogbernabe@gmail.com>
2024-11-11 13:13:52 +0100

    Inicio del proyecto. Botones de ciclo1 y ciclo2 creados y routeados en el archivo app.routes.ts

---
+++ src/app/ciclo1/ciclo1.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ciclo1',
+  standalone: true,
+  imports: [],
+  templateUrl: './ciclo1.component.html',
+  styleUrl: './ciclo1.component.css'
+})
+export class Ciclo1Component {
+
+}
---
+++ src/app/ciclo2/ciclo2.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ciclo2',
+  standalone: true,
+  imports: [],
+  templateUrl: './ciclo2.component.html',
+  styleUrl: './ciclo2.component.css'
+})
+export class Ciclo2Component {
+
+}
