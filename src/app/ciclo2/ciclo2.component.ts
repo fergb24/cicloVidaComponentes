5f8fe33b143bff43371fe2749cbfe198636f33a5
Fernando García <fernandogbernabe@gmail.com>
2024-11-12 08:48:47 +0100

    Estructuramos el componente ciclo2 que actuará como componente padre

--- src/app/ciclo2/ciclo2.component.ts
+++ src/app/ciclo2/ciclo2.component.ts
@@ -1,18 +1,27 @@
 import { Component } from '@angular/core';
 import { MensajesService } from '../services/mensajes.service';
+import { ContadorComponent } from "../contador/contador.component";
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-ciclo2',
   standalone: true,
-  imports: [],
+  imports: [ContadorComponent, NgIf],
   templateUrl: './ciclo2.component.html',
   styleUrl: './ciclo2.component.css'
 })
 export class Ciclo2Component {
 
+  mostrarContador = true;
+  inicial = 0;
+
   constructor(private mensajesService: MensajesService) {}
 
   ngOnInit() {
     console.log(this.mensajesService.getMensaje());
+  }
+
+  handleDestruccion() {
+    console.log('PadreComponent: El contador fue destruido');    
   }
 }
